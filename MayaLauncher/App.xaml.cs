2a12cca38f3f3d1c61b958f00dfc3144be7a5be8
Willem Kokke <mail@willem.net>
2020-03-04 14:19:25 +0000

    Allow .ma and .mb extensions to be associated with the launcher or any particular installed version
    Extracting minimal required maya version from .ma files, and started on .mb files

--- MayaLauncher/App.xaml.cs
+++ MayaLauncher/App.xaml.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Reflection;
 using System.Text.Json;
 using System.Threading;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Shell;
 
 namespace MayaLauncher
 {
+    public enum LaunchType
+    {
+        VersionFromFile,
+        LatestVersion,
+        ChooseVersion,
+    }
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
@@ -18,8 +25,6 @@ namespace MayaLauncher
         public static Mutex Mutex { get; set; }
 
         public NamedPipeManager PipeManager { get; set; }
-
-        public MayaVersion[] MayaVersions { get; set; }
 
         public App()
         {
@@ -41,7 +46,13 @@ namespace MayaLauncher
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            MayaVersions = MayaVersion.FindAll();
+            Maya.Initalise();
+
+            if (Maya.Versions.Count > 0)
+            {
+                FileAssociation.AssociateWithLauncher();
+                //FileAssociation.AssociateWithMayaVersion(Maya.LatestVersion);
+            }
 
             CreateJumpList();
 
@@ -68,7 +79,7 @@ namespace MayaLauncher
             JumpList jumpList = new JumpList();
             JumpList.SetJumpList(Application.Current, jumpList);
 
-            foreach(var v in MayaVersions)
+            foreach(var v in Maya.Versions)
             {
                 JumpTask mayaTask = new JumpTask();
                 mayaTask.Title = v.Name;
@@ -95,37 +106,66 @@ namespace MayaLauncher
 
             if ((args.Count > 2))
             {
+                string command = args[1].ToLowerInvariant();
+
                 //the first index always contains the location of the exe so we need to check the second index
-                if ((args[1].ToLowerInvariant() == "/maya"))
+                if (command == "/maya")
                 {
-                    using (Process mayaProcess = new Process())
+                    string version = args[2];
+                    Maya.Launch(version, AdjustEnvironment);
+                }
+                else if(command == "/open")
+                {
+                    string filename = args[2];
+                    LaunchType launchType = LaunchType.VersionFromFile;
+
+                    if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
                     {
-                        foreach (var v in MayaVersions)
-                        {
-                            if (v.Name == args[2])
-                            {
-                                var path = Path.Combine(v.Path, "bin", "maya.exe");
-                                mayaProcess.StartInfo.UseShellExecute = false;
-                                mayaProcess.StartInfo.FileName = path;
-                                var env = mayaProcess.StartInfo.Environment;
-                                PrependToEnvironment(env, "MAYA_MODULE_PATH", @"E:\Projects\humain-mayatools;");
-                                PrependToEnvironment(env, "MAYA_DISABLE_CIP", "1");
-                                PrependToEnvironment(env, "MAYA_DISABLE_CER", "1");
-                            }
-                        }
-                        mayaProcess.Start();
+                        launchType = LaunchType.LatestVersion;
+                    }
+                    else if (Keyboard.IsKeyDown(Key.LeftAlt) || Keyboard.IsKeyDown(Key.RightAlt))
+                    {
+                        launchType = LaunchType.LatestVersion;
+                    }
+
+                    var fileVersion = MayaFileVersion.FromFile(filename);
+                    if (fileVersion != null)
+                    {
+                        Debug.WriteLine(fileVersion.Requires);
+                        Debug.WriteLine(fileVersion.Product);
+                        Debug.WriteLine(fileVersion.Version);
+                        Debug.WriteLine(fileVersion.Cut);
+                    }
+
+                    switch (launchType)
+                    {
+                        case LaunchType.VersionFromFile:
+                            break;
+                        case LaunchType.ChooseVersion:
+                            break;
+                        case LaunchType.LatestVersion:
+                            Maya.Launch(Maya.LatestVersion, AdjustEnvironment, filename);
+                            break;
                     }
                 }
             }
         }
 
-        private void PrependToEnvironment(IDictionary<string, string> env, string variable, string value)
+        private void AdjustEnvironment(IDictionary<string, string> environment)
         {
-            if (env.ContainsKey(variable))
+            environment["MAYA_DISABLE_CIP"] = "1";
+            environment["MAYA_DISABLE_CER"] = "1";
+
+            PrependToEnvironment(environment, "MAYA_MODULE_PATH", @"E:\Projects\humain-mayatools");
+        }
+
+        private static void PrependToEnvironment(IDictionary<string, string> environment, string variable, string value)
+        {
+            if (environment.ContainsKey(variable))
             {
-                value += env[variable];
+                value += (environment[variable] + ";");
             }
-            env[variable] = value;
+            environment[variable] = value;
         }
     }
 }
---
+++ MayaLauncher/MayaFileVersion.cs
@@ -0,0 +1,98 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace MayaLauncher
+{
+    public class MayaFileVersion
+    {
+        public string Requires { get; internal set; }
+        public string Product { get; internal set; }
+        public string Version { get; internal set; }
+        public string Cut { get; internal set; }
+
+        public static MayaFileVersion FromFile(string mayaFile)
+        {
+            if (File.Exists(mayaFile))
+            {
+                var extension = Path.GetExtension(mayaFile).ToLower();
+
+                switch (extension)
+                {
+                    case ".ma":
+                        return ParseMayaAsciiFile(mayaFile);
+                    case ".mb":
+                        return ParseMayaBinaryFile(mayaFile);
+                }
+            }
+            
+            return null;
+        }
+
+        private static MayaFileVersion ParseMayaAsciiFile(string file)
+        {
+            try 
+            {
+                var result = new MayaFileVersion();
+
+                using (var stream = new StreamReader(file))
+                {
+                    string requires = "requires maya ";
+                    string product = "fileInfo \"product\" ";
+                    string version = "fileInfo \"version\" ";
+                    string cut = "fileInfo \"cutIdentifier\" ";
+                    string createNode = "createNode ";
+
+                    string line = "";
+                    while ((line = stream.ReadLine()) != null)
+                    {
+                        if (line.StartsWith(requires))
+                        {
+                            result.Requires = line.Replace(requires, "").Trim(';').Trim('"');
+                        }
+                        else if (line.StartsWith(product))
+                        {
+                            result.Product = line.Replace(product, "").Trim(';').Trim('"');
+                        }
+                        else if (line.StartsWith(version))
+                        {
+                            result.Version = line.Replace(version, "").Trim(';').Trim('"');
+                        }
+                        else if (line.StartsWith(cut))
+                        {
+                            result.Cut = line.Replace(cut, "").Trim(';').Trim('"');
+                        }
+                        else if (line.StartsWith(createNode))
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                return result;
+            }
+            catch (System.Exception e)
+            {
+                Debug.WriteLine($"Failed to open file: {file} ({e.Message})");
+                return null;
+            }
+        }
+
+        private static MayaFileVersion ParseMayaBinaryFile(string file)
+        {
+            try
+            {
+                var result = new MayaFileVersion();
+
+                using (BinaryReader writer = new BinaryReader(File.Open(file, FileMode.Open)))
+                {
+                }
+                return result;
+            }
+            catch (System.Exception e)
+            {
+                Debug.WriteLine($"Failed to open file: {file} ({e.Message})");
+                return null;
+            }
+        }
+    }
+}
